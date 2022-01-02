8617b58aa59adb4970c8ef1e26b764512ae7c209
RomanZhakhovTR <fpbeat@gmail.com>
2022-01-02 18:30:18 +0200

    Replace custom scrolling to scrollIntoView

--- src/scripts/components/list.ts
+++ src/scripts/components/list.ts
@@ -1,6 +1,4 @@
-import { SCROLLING_SPEED } from '../constants';
-
-export default class List {
+  export default class List {
   element: HTMLElement;
 
   scrollPos: number;
@@ -33,71 +31,14 @@ export default class List {
     this.element.scrollTop = 0;
   }
 
-  scrollToChildElement(element: HTMLElement, direction: 1 | -1, animatedScroll: boolean): void {
+  scrollToChildElement(element: HTMLElement, direction: 1 | 0 | -1, animatedScroll: boolean): void {
     if (!element) {
       return;
     }
 
-    const listHeight = this.element.offsetHeight;
-    // Scroll position of dropdown
-    const listScrollPosition = this.element.scrollTop + listHeight;
+    const scrollBehavior = animatedScroll ? 'smooth' : 'auto';
+    const scrollVerticalBlock = {'0': 'center', '1': 'end', '-1': 'start'};
 
-    const elementHeight = element.offsetHeight;
-    // Distance from bottom of element to top of parent
-    const elementPos = element.offsetTop + elementHeight;
-
-    // Difference between the element and scroll position
-    const destination =
-      direction > 0
-        ? this.element.scrollTop + elementPos - listScrollPosition
-        : element.offsetTop;
-
-    if (animatedScroll) {
-      requestAnimationFrame(() => {
-        this._animateScroll(destination, direction);
-      });
-    } else {
-      this.element.scrollTop = destination;
-    }
-  }
-
-  _scrollDown(scrollPos: number, strength: number, destination: number): void {
-    const easing = (destination - scrollPos) / strength;
-    const distance = easing > 1 ? easing : 1;
-
-    this.element.scrollTop = scrollPos + distance;
-  }
-
-  _scrollUp(scrollPos: number, strength: number, destination: number): void {
-    const easing = (scrollPos - destination) / strength;
-    const distance = easing > 1 ? easing : 1;
-
-    this.element.scrollTop = scrollPos - distance;
-  }
-
-  _animateScroll(destination: number, direction: number): void {
-    const strength = SCROLLING_SPEED;
-    const choiceListScrollTop = this.element.scrollTop;
-    let continueAnimation = false;
-
-    if (direction > 0) {
-      this._scrollDown(choiceListScrollTop, strength, destination);
-
-      if (choiceListScrollTop < destination) {
-        continueAnimation = true;
-      }
-    } else {
-      this._scrollUp(choiceListScrollTop, strength, destination);
-
-      if (choiceListScrollTop > destination) {
-        continueAnimation = true;
-      }
-    }
-
-    if (continueAnimation) {
-      requestAnimationFrame(() => {
-        this._animateScroll(destination, direction);
-      });
-    }
+    element.scrollIntoView({block: scrollVerticalBlock[String(direction)], behavior: scrollBehavior});
   }
 }
